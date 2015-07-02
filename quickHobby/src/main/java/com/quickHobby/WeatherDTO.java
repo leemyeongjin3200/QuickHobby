192e7adcc633ce0ec06447e34024e9ee688ff11a
seoingoo <disk012@naver.com>
2015-07-02 17:40:13 +0900

    quickHobby.ingoo.0729

---
+++ quickHobby/src/main/java/com/quickHobby/WeatherDTO.java
@@ -0,0 +1,62 @@
+package com.quickHobby;
+
+public class WeatherDTO {
+	private String city,mode,tmEf,wf,tmn,tmx,reliability;
+	 
+    public String getCity() {
+        return city;
+    }
+ 
+    public void setCity(String city) {
+        this.city = city;
+    }
+ 
+    public String getMode() {
+        return mode;
+    }
+ 
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+ 
+    public String getTmEf() {
+        return tmEf;
+    }
+ 
+    public void setTmEf(String tmEf) {
+        this.tmEf = tmEf;
+    }
+ 
+    public String getWf() {
+        return wf;
+    }
+ 
+    public void setWf(String wf) {
+        this.wf = wf;
+    }
+ 
+    public String getTmn() {
+        return tmn;
+    }
+ 
+    public void setTmn(String tmn) {
+        this.tmn = tmn;
+    }
+ 
+    public String getTmx() {
+        return tmx;
+    }
+ 
+    public void setTmx(String tmx) {
+        this.tmx = tmx;
+    }
+ 
+    public String getReliability() {
+        return reliability;
+    }
+ 
+    public void setReliability(String reliability) {
+        this.reliability = reliability;
+    }
+    
+}
